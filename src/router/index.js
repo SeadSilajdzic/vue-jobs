f90e14610290e426f867468db9f69bf82ddf8589
SeadSilajdzic <sead.silajdzic@elevatebits.com>
2024-06-11 10:35:21 +0200

    renamed JobsView.vue to Jobs.vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from "@/views/AboutView.vue";
-import JobsView from "@/views/jobs/JobsView.vue";
+import Jobs from "@/views/jobs/Jobs.vue";
 
 const routes = [
   {
@@ -17,7 +17,7 @@ const routes = [
   {
     path: '/jobs',
     name: 'Jobs',
-    component: JobsView
+    component: Jobs
   }
 ]
 
