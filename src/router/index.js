6176d54bb7ac637a76c2d6044e0ef085a4262cdd
SeadSilajdzic <sead.silajdzic@elevatebits.com>
2024-06-11 11:03:01 +0200

    job detail links

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,36 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from "@/views/AboutView.vue";
 import Jobs from "@/views/jobs/Jobs.vue";
+import JobDetails from "@/views/jobs/JobDetails.vue";
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'About',
-    component: AboutView
-  },
-  {
-    path: '/jobs',
-    name: 'Jobs',
-    component: Jobs
-  }
+    {
+        path: '/',
+        name: 'Home',
+        component: HomeView
+    },
+    {
+        path: '/about',
+        name: 'About',
+        component: AboutView
+    },
+    {
+        path: '/jobs',
+        name: 'Jobs',
+        component: Jobs
+    },
+    {
+        path: '/jobs/:id',
+        name: 'JobDetails',
+        component: JobDetails,
+        props: true
+    }
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes
 })
 
 export default router
