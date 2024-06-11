29e5736e64e37c499d1062518bef7561f6f6a65b
SeadSilajdzic <sead.silajdzic@elevatebits.com>
2024-06-11 11:08:09 +0200

    catch 404 and redirect

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import HomeView from '../views/HomeView.vue'
 import AboutView from "@/views/AboutView.vue";
 import Jobs from "@/views/jobs/Jobs.vue";
 import JobDetails from "@/views/jobs/JobDetails.vue";
+import NotFound from "@/views/NotFound.vue";
 
 const routes = [
     {
@@ -25,6 +26,17 @@ const routes = [
         name: 'JobDetails',
         component: JobDetails,
         props: true
+    },
+    // Redirect from all-jobs (non-existing) route to jobs
+    {
+        path: '/all-jobs',
+        redirect: '/jobs'
+    },
+    // Catch all 404
+    {
+        path: '/:catchAll(.*)',
+        name: NotFound,
+        component: NotFound
     }
 ]
 
